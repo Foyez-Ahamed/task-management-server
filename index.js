2c141c575f3849673686a24c579409b3371a5510
Foyez-Ahamed <foyez1435@gmail.com>
2023-12-22 12:43:33 +0600

    successfully connect with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -10,8 +11,48 @@ app.use(express.json())
 
 
 
+// mongodb //
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wslenxe.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // operations //
+
+
+
+    // operations //
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongodb //
+
+
+
 app.get('/', (req, res) => {
-    res.send('Task management Server');
+    res.send('Task management Server is running');
   })
   app.listen(port, () => {
     console.log(`App is running on ${port}`);
