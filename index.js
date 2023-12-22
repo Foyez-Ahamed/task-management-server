b5e617f3c342d8c496036ab7d92759143bb7d2bb
Foyez-Ahamed <foyez1435@gmail.com>
2023-12-22 19:11:55 +0600

    task deleted route create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -48,6 +48,13 @@ async function run() {
         const result = await taskCollections.find(query).toArray();
         res.send(result)
     })
+
+    app.delete('/api/removeTask/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id) };
+        const result = await taskCollections.deleteOne(query);
+        res.send(result);
+      })
     // CRUD // 
 
 
